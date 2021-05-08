397b7b19751ebecdb23c506284684d0f5581d757
Dmitry-flow <dms9961@gmail.com>
2021-05-08 22:49:21 +0300

    color changed

--- WeatherApp/Source/Flow/Main/MainViewController.swift
+++ WeatherApp/Source/Flow/Main/MainViewController.swift
@@ -11,7 +11,7 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .green
     }
     
     func inject(presenter: MainPresenter) {
